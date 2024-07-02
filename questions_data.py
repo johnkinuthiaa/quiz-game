32cb6c3b6d772190706c9752bc323451036b0569
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-07-02 09:13:03 +0300

    initial commit

---
+++ questions_data.py
@@ -0,0 +1,13 @@
+question_data = [
+    {"text":"A slug's blood is green.", "answer":"true"},
+    {"text":"The loudest animal is the african elephant.", "answer":"false"},
+    {"text":"approximately one quater of human bones are in the feet.", "answer":"true"},
+    {"text":"The total surface area of a human lungs is the size of a football pitch.", "answer":"true"},
+    {"text":"in west virginia if you accidentally hit an animal you are allowed to take it home and eat it .", "answer":"true"},
+    {"text":"It's illegal to pee in the ocean in portugal.", "answer":"true"},
+    {"text":"you can lead a cow downstairs but not upstairs.", "answer":"flase"},
+    {"text":"Google was originally called 'Backrub'.", "answer":"true"},
+    {"text":"buzz was the first guy on the moon.", "answer":"true"},
+    {"text":"A few ounces of chocolate can kill a dog.", "answer":"true"},
+    {"text":"A piece of paper cannot be folded in half more than seven times.", "answer":"false"},
+]
